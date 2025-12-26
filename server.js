2dedde166a8d6bb9136ae8a82ff78c9fead4118f
fayzullaevfarrukh98 <fayzullaevfarrukh98@gmail.com>
2025-12-26 19:27:02 +0900

    feat:portfolio publishing

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+})
 
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 
 // 2: Session
 // 3: Veiws code
@@ -15,8 +26,11 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
+  // TODO: code with here 
+});
+
+app.get('/author', (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
